6a8cdcabf5b03af10259080a66a2d3f2ff1492ec
Try <50934786+TRYblog@users.noreply.github.com>
2021-03-14 14:34:59 +0800

    Update add.go

--- root/add.go
+++ root/add.go
@@ -1,6 +1,7 @@
 package root
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -8,7 +9,7 @@ import (
 
 func Add(listenport string, address string, connectport string, connectaddress string) { //添加转发规则
 	cmdstr := Cmd("netsh interface portproxy show all")
-	if strings.Contains(cmdstr, connectaddress+"    "+connectport) == true && strings.Contains(cmdstr, connectaddress+"    "+listenport) == true {
+	if strings.Contains(cmdstr, connectaddress) && strings.Contains(cmdstr, connectport) && strings.Contains(cmdstr, connectaddress) && strings.Contains(cmdstr, listenport) {
 		log.Println("[Info] 监听ip:", connectaddress, " 端口:", listenport)
 		log.Println("[Info] 转发ip:", connectaddress, " 转发端口:", connectport)
 		log.Println("[Info] 该规则已经添加过了！")
@@ -16,7 +17,8 @@ func Add(listenport string, address string, connectport string, connectaddress s
 	} else {
 		Cmd("netsh interface portproxy add v4tov4 listenport=" + listenport + " listenaddress=" + connectaddress + " connectport=" + connectport + " connectaddress=" + connectaddress)
 		c1 := Cmd("netsh interface portproxy show all")
-		if strings.Contains(c1, connectaddress+"    "+connectport) == true {
+		fmt.Println(c1)
+		if strings.Contains(c1, connectaddress) == true && strings.Contains(c1, connectport) {
 			log.Println("[Info] 监听ip:", address, " 端口:", listenport)
 			log.Println("[Info] 转发ip:", connectaddress, " 转发端口:", connectport)
 			log.Println("[Info] 添加成功！")
